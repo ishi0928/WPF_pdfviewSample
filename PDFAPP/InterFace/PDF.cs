45c2e8147334bdec8b6b8e326473f557b6fe13f2
ishi0928 <so_ishihara@star-system.jp>
2025-10-31 09:08:43 +0900

    Add files via upload

---
+++ PDFAPP/InterFace/PDF.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace PDFAPP
+{
+
+    //インターフェース
+    public interface pdfApp
+    {
+        public string encoding { get;}
+        public char delimiter { get;}
+        public string filepath { get;}
+    }
+
+    //実装
+    public class PDF : pdfApp
+    {
+
+        public string encoding { get; private set; }
+        public char delimiter { get; private set; }
+        public string filepath { get; private set; }
+
+        public PDF()
+        {
+            encoding = "utf-8";
+            delimiter = ',';
+            filepath = @"C:\PDFAPP\作業内容.pdf";
+        }
+    }
+}
---
+++ PDFAPP/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Web.WebView2.Wpf;
+using Microsoft.Web.WebView2.Core;
+
+
+namespace PDFAPP
+{
+    public partial class MainWindow : Window
+    {
+        private readonly pdfApp _pdfApp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _pdfApp = new PDF();
+
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // PDFを表示
+            await DisplayPdfAsync();
+        }
+
+        private async Task DisplayPdfAsync()
+        {
+            await pdfViewer.EnsureCoreWebView2Async();
+
+            if (System.IO.File.Exists(_pdfApp.filepath))
+            {
+                string filePath = _pdfApp.filepath.Replace("\\", "/");
+                pdfViewer.CoreWebView2.Navigate($"file:///{filePath}");
+            }
+            else
+            {
+                MessageBox.Show($"PDFが見つかりません。\n{_pdfApp.filepath}");
+            }
+        }
+    }
+}
